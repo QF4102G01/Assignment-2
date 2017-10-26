a738b9ca741bacc39b578f7e8234c1f60c5d4982
xmchenph <xmchenph@gmail.com>
2017-10-27 00:52:03 +0800

    comments for A2p2

--- Main/A2p2.m
+++ Main/A2p2.m
@@ -1,23 +1,31 @@
 %Group members: Chen Penghao, Wang Zexin
 %Group number: G01
 
-explicitIIIFDM(9.8, 9, 0.001, 0.25, 0.01, 0.15, 25, 0.05)
-
+% Initialize values for FDM explicit III
 S0 = 9.8;
 X = 9;
 T = 0.25;
 r = 0.001;
 q = 0.01;
 sigma = 0.15;
-N = 25;
+N = 2900;
 ds = 0.05;
 
+% Execute FDM explicit III
+run1 = explicitIIIFDM(S0, X, r, T, q, sigma, N, ds);
+
+% Determine a lower bound for N = T/deltaT
 Smax = 4 * X;
 I = floor(Smax / ds);
+N_lb = T * (sigma ^ 2 * I ^ 2 + r * I);
 
-N = T * (sigma ^ 2 * I ^ 2 + r * I);
+% Rerun explicit III FDM with the value of lower bound N
+run2 = explicitIIIFDM(S0, X, r, T, q, sigma, N_lb, ds);
 
-explicitIIIFDM(9.8, 9, 0.001, 0.25, 0.01, 0.15, 2916, 0.05)
+% Rerun explicit III FDM and lower N gradually and locate the cut-off value
+% of N where the estimate loses all its significant figures
+N_co = 2916;
+run3 = explicitIIIFDM(S0, X, r, T, q, sigma, N_co, ds);
 
-
-explicitIIIFDMAmericanCall(9.8, 9, 0.001, 0.25, 0.01, 0.15, 2900, 0.05)
+% Run explicit III FDM on American vanilla call option
+run4 = explicitIIIFDMAmericanCall(S0, X, r, T, q, sigma, N, ds);
