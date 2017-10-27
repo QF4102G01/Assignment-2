f928917ba9bf18e0260fb3fbd9c4d4ca3dd678e4
xmchenph <xmchenph@gmail.com>
2017-10-27 19:49:09 +0800

    add comments and report writing (u/c)

--- Main/A2p1.m
+++ Main/A2p1.m
@@ -1,6 +1,16 @@
 %Group members: Chen Penghao, Wang Zexin
 %Group number: G01
 
+% Initialize the option parameters
+runningTime = 0.25;
+tau = 0.25;
+S0 = 100;
+sigma = 0.4;
+q = 0.01;
+runningAverage = 95;
+r = 0.1;
+K = 100;
+
 % Initialize possible rho values
 rhos = [1 1/2 1/5];
 
@@ -11,8 +21,11 @@ Ns = [50 100 200 400];
 % with the given values of rho and N
 for rho = rhos
     for N = Ns
-        disp(['For rho = ', num2str(rho), ' and N = ', num2str(N), '. The option price is : ']);
-        FSGMAmericanFixedStrikeAsianPut(0.25, 0.25, 100, 0.4, 0.01, 95, 0.1, 100, N, rho)
+        stamp1 = now;
+        result = FSGMAmericanFixedStrikeAsianPut(runningTime, tau, S0, sigma, q, runningAverage, r, K, N, rho);
+        stamp2 = now;
+        disp(['For rho = ', num2str(rho), ' and N = ', num2str(N), '. The option price is : ', num2str(result)]);
+        disp(['Time taken to finish this run is ', num2str(stamp2-stamp1)]);
     end
 end
 
--- Main/FSGMAmericanFixedStrikeAsianPut.m
+++ Main/FSGMAmericanFixedStrikeAsianPut.m
@@ -2,7 +2,7 @@
 %Group number: G01
 
 function [optionValue] = FSGMAmericanFixedStrikeAsianPut(runningTime, tau, S0, sigma, q, runningAverage, r, K, N, rho)
-
+    % Initializing the parameters
     deltaT = tau / N;
     deltaX = sigma * sqrt(deltaT);
     deltaY = rho * deltaX;
@@ -11,11 +11,15 @@ function [optionValue] = FSGMAmericanFixedStrikeAsianPut(runningTime, tau, S0, s
     p = (exp(r * deltaT) - d) / (u-d);
     m = 1 / rho;
     
+    % Include the price at t = 0.25 in the Running Average
     runningN = runningTime / deltaT + 1;
     runningAverage = (runningAverage * (runningN - 1) + S0) / runningN;
+    
+    % Initialize shifts in j and k values
     jshift = 1;
     kshift = N * m + 1;
-
+    
+    % Initialize terminal payoff
     v = zeros(N+1, 2 * N * m + 1);
     for j = 0 : N
         for k = -N * m : N * m
@@ -24,32 +28,45 @@ function [optionValue] = FSGMAmericanFixedStrikeAsianPut(runningTime, tau, S0, s
         end
     end
     
+    % Backward iterations
     for n = N-1 : -1 : 0
         newV = zeros(N+1, 2 * N * m + 1);
         for j = 0 : n
             for k = -n * m : n * m
                 S = S0 * exp((2 * j - n) * deltaX);
                 A = runningAverage * exp(k * deltaY);
+                
+                % Running average for the up and down case
                 Au = (A * (n + runningN) + S * u) / (n + runningN + 1);
                 Ad = (A * (n + runningN) + S * d) / (n + runningN + 1);
+                
+                % Obtain the index in the FSGM
                 ku = min(floor(log(Au / runningAverage) / deltaY), N * m-1);
                 kd = min(floor(log(Ad / runningAverage) / deltaY), N * m-1);
+                
                 ju = j + 1;
                 jd = j;
+                
+                % Interpolate to get the payoff for up case
                 Auup = runningAverage * exp((ku+1) * deltaY);
                 Audown = runningAverage * exp(ku * deltaY);
                 alphau = (Auup - Au) / (Auup - Audown);
                 Vu = (1 - alphau) * v(ju+jshift, ku+1+kshift) + alphau * v(ju+jshift, ku+kshift);
+                
+                % Interpolate to get the payoff for the down case
                 Adup = runningAverage * exp((kd+1) * deltaY);
                 Addown = runningAverage * exp(kd * deltaY);
                 alphad = (Adup - Ad) / (Adup - Addown);
                 Vd = (1 - alphad) * v(jd+jshift, kd+1+kshift) + alphad * v(jd+jshift, kd+kshift);
+                
+                % Obtain the option value for that point
                 newV(j+jshift, k+kshift) = max(exp(-r * deltaT) * (p * Vu + (1-p) * Vd), K - A);
             end
         end
         v = newV;
     end
     
+    % Final result
     optionValue = max(v(jshift, kshift), K - runningAverage);
     
 end
